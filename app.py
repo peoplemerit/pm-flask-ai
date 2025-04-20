1a1e2d54b8563354e695c8abc283ba5ee78905e9
peoplemerit <peoplemerit@gmail.com>
2025-04-20 08:45:11 -0400

    Add files via upload
    new file replacements. April 20 2025, 8:45AM

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
+
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from PIL import Image
 import pytesseract
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS
 
 @app.route("/")
 def home():
