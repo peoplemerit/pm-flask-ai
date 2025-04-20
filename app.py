4fe8facf3e4fb2a113194cd4f6bcba603a0341d9
peoplemerit <peoplemerit@gmail.com>
2025-04-20 12:20:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from PIL import Image
 import pytesseract
+
+# Explicitly set Tesseract path for Render deployment
+pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS
